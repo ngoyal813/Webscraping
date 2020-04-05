7ea9f26cb7d388595781a53fc437c60052dbbd5e
Ankit-Gupta-1511 <ankit.gupta.cs1511@gmail.com>
2020-04-05 16:34:07 +0530

    updated webscraping script

--- Webscraping.py
+++ Webscraping.py
@@ -4,23 +4,32 @@ from bs4 import BeautifulSoup
 from lxml import html
 import lxml
 
+
+def get_views(url):
+    r = requests.get(url).text
+    soup = BeautifulSoup(r, 'lxml')
+    count_container = soup.find(class_="watch-view-count")
+    print(count_container.text)
+
+    return count_container.text
+
+
 video_details = ()
-data = pd.read_excel("V1.xlsx")
+data = pd.read_excel("Links.xlsx")
 for i in data.index:
- d=()
- d = data.values[i]
- for url in d:
-     r = requests.get(url).text
-     c = BeautifulSoup(r,'lxml')
-     #print("data is", repr(c.find(class_= 'watch-view-count')))
-     try:
-         for div in c.find(class_= 'watch-view-count'):
-           video_details = div.replace(",", "").rstrip('views')
-           print(video_details)
-     except:
-         try:
-             for div in c.find(class_= 'watch-view-count'):
-                video_details = div.replace(",", "").rstrip('views')
-                print(video_details)
-         except:
-            print(" ")
+    d = ()
+    d = data.values[i]
+    for url in d:
+        print("")
+        print(url)
+        try:
+            views = get_views(url)
+        except:
+            try:
+                views = get_views(url)
+            except:
+                try:
+                    views = get_views(url)
+                except:
+                    print("Failed to fetch")
+
